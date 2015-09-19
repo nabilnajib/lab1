0e1232f75be1e264d5408ad85febcb54137e4503
nabil <nabil.tahri@polymtl.ca>
2015-09-19 16:12:01 -0400

    modification sur le tout du tout reste le footer qui donne une comportement bizare pour le formulaire

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Accueil' });
 });
-
+router.get('/form', function(req, res, next) {
+  res.render('form', { title: 'Formulaire' });
+});
+router.get('/test', function(req, res, next) {
+  res.render('test', { title: 'Aide' });
+});
 module.exports = router;
